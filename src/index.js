3835cfd1461f9a18d1d191ecddb7289837d7c15f
Schreezer <96696271+Schreezer@users.noreply.github.com>
2023-04-17 02:36:40 +0530

    Wasupp
    hello

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
@@ -7,6 +7,8 @@ import { Root } from "./routes/root";
 import { SignAndSubmitTx } from "./routes/sign-and-submit-tx";
 import { SwitchAccount } from "./routes/switch-account";
 import { User } from "./routes/user";
+
+
 
 const router = createBrowserRouter([
   {
@@ -26,12 +28,18 @@ const router = createBrowserRouter([
         path: "/switch-account",
         element: <SwitchAccount />,
       },
+      // {
+      //   path: "/questions",
+      //   element: <
+
+      // },
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* < Sidebar /> */}
     <RouterProvider router={router} />
   </React.StrictMode>
 );
